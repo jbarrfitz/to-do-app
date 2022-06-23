eee46d2db4ae7b580d8dbce9174bfbd1433d7eff
Jerry Barrows-Fitzgerald <jerry.barrows-fitzgerald@dpsmedia.com>
2022-06-23 10:44:57 -0500

    re-rendered to-do list with styled components

--- pages/styled-components.tsx
+++ pages/styled-components.tsx
@@ -3,28 +3,137 @@
    looks under the hood (look at source files, etc.), specifically for the 
    theme provider */
 
-import type { NextPage } from "next";
-import { useState } from "react";
-import styled from "styled-components";
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
+
+interface Task {
+  id: string;
+  desc: string;
+  isDone: boolean;
+}
+
+const PageTitle = styled.h1`
+  font-size: 1.5rem;
+  line-height: 2rem;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 1.25rem;
+`;
+const UserControls = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 15px;
+
+  input {
+    border-width: 0;
+    box-shadow: 10px 5px 5px slategray;
+    padding: 0.25rem;
+  }
+
+  button {
+    border-width: 2px;
+    border-radius: 0.5rem;
+    padding: 0.25rem;
+    background-color: rgb(214, 211, 209);
+  }
+`;
+const TaskItem = styled.li<{ isFinished: boolean }>`
+  margin: 5px;
+  padding: 2px;
+  text-align: center;
+  background: ${(prop) => (prop.isFinished ? '#ffcccb' : '#90ee90')};
+`;
 
 const Card = styled.div`
   padding: 1.5rem;
   color: inherit;
   text-decoration: none;
-  border: 1px solid black;
-  border-radius: 10px;
-  transition: color 0.15s ease, border-color 0.15s ease;
+  border: 2px solid black;
+  border-radius: 5px;
   width: 100%;
-  &:hover,
-  :focus,
-  :active {
-    color: #0070f3;
-    border-color: #0070f3;
-  }
+  max-width: 600px;
+  margin: auto;
 `;
 
 const Home: NextPage = () => {
-  return <Card>Hello World! This is a styled-component</Card>;
+  const [task, setTask] = useState<Task>({
+    id: '',
+    desc: '',
+    isDone: false,
+  });
+
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  function handleChange(e: React.FormEvent<HTMLInputElement>) {
+    e.currentTarget.value &&
+      setTask({
+        id: '',
+        desc: e.currentTarget.value,
+        isDone: false,
+      });
+  }
+
+  function handleAdd() {
+    const newTask: Task = {
+      id: uuidv4(),
+      desc: task.desc,
+      isDone: task.isDone,
+    };
+    setTaskList([newTask, ...taskList]);
+    setTask({
+      id: '',
+      desc: '',
+      isDone: false,
+    });
+  }
+
+  function handleToggle(id: string) {
+    const newTaskList = taskList.map((task) => {
+      if (task.id === id) {
+        const newTask = {
+          ...task,
+          isDone: !task.isDone,
+        };
+        return newTask;
+      }
+      return task;
+    });
+    setTaskList(newTaskList);
+  }
+
+  function handleDelete() {
+    const newTaskList = taskList.filter((task) => !task.isDone);
+    setTaskList(newTaskList);
+  }
+
+  const tasks = taskList.map(({ id, desc, isDone }) => {
+    return (
+      <TaskItem isFinished={isDone} onClick={() => handleToggle(id)} key={id}>
+        {desc}
+      </TaskItem>
+    );
+  });
+
+  return (
+    <Card>
+      <PageTitle>To Do List App</PageTitle>
+      <UserControls>
+        <input
+          type='text'
+          className='task-input'
+          placeholder='Enter task'
+          onChange={handleChange}
+          value={task.desc}
+        />
+        <button onClick={handleAdd}>Add New Task</button>
+        <button onClick={handleDelete}>Delete Completed</button>
+      </UserControls>
+      <ul>{tasks}</ul>
+    </Card>
+  );
 };
 
 export default Home;
